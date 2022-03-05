054f4d5ee05afded22a5bfdea464eddd2c070b55
Nicholi Caron <nmcaron@protonmail.ch>
2022-03-05 06:08:37 +0000

    Add files via upload

---
+++ p121/stox.go
@@ -0,0 +1,8 @@
+package stox
+
+// Takes an array of stock prices, indexed by the day that price is available
+// finds max difference (profit) between indexed values (prices on diff days)
+
+func maxProfit(prices []int) int {
+
+}
