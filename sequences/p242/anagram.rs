7e533cbce73d9cbfaba726bdb3ed278246139a89
Nicholi Caron <nmcaron@protonmail.ch>
2022-03-11 17:46:12 +0000

    Update anagram.rs

--- sequences/p242/anagram.rs
+++ sequences/p242/anagram.rs
@@ -1,18 +1,17 @@
-// This isn't very fast, or very good on memory, but I think it reads much nicer 
-// than more efficient solutions. Improving efficiency would require a more imperative solution, 
-// removing iterator adaptors
+// Note to self: sort_unstable() results in considerable speedups, so when dealing with vectors of primitive types,
+// prefer unstable sort over stable sort [thanks clippy]
 
 // takes a string, breaks it into characters, casts the chars to u32 values (in case of unicode)
 // then collects them into a vector. This vector is then sorted to compare to the other
 // parameter
 fn is_anagram(s: String, t: String) -> bool {
     let mut s_vec: Vec<u32> = s.chars().map(|letter| letter as u32).collect();
-    s_vec.sort();
+    s_vec.sort_unstable();
 
     let mut t_vec: Vec<u32> = t.chars().map(|letter| letter as u32).collect();
-    t_vec.sort();
+    t_vec.sort_unstable();
 
-    return s_vec == t_vec
+    s_vec == t_vec
     
 }
 
