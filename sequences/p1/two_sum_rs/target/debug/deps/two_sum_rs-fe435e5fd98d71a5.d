5d8e4740107875e37744e3daf6ea09c17ca58039
Nicholi <nmcaron@protonmail.ch>
2022-11-04 09:41:24 -0400

    edit gitignore

---
+++ sequences/p1/two_sum_rs/target/debug/deps/two_sum_rs-fe435e5fd98d71a5.d
@@ -0,0 +1,5 @@
+/home/c/dev/projects/leetcode/sequences/p1/two_sum_rs/target/debug/deps/two_sum_rs-fe435e5fd98d71a5: src/main.rs
+
+/home/c/dev/projects/leetcode/sequences/p1/two_sum_rs/target/debug/deps/two_sum_rs-fe435e5fd98d71a5.d: src/main.rs
+
+src/main.rs:
---
+++ sequences/p1/two_sum_rs/target/debug/two_sum_rs.d
@@ -0,0 +1,1 @@
+/home/c/dev/projects/leetcode/sequences/p1/two_sum_rs/target/debug/two_sum_rs: /home/c/dev/projects/leetcode/sequences/p1/two_sum_rs/src/main.rs
